e56f59e8a0f27e18fcb3ac3f2b8847ecabb3588b
ssk8 <stinkyskate@pm.me>
2019-08-10 18:52:22 -0500

    added auto torrent update

--- clipper.py
+++ clipper.py
@@ -1,21 +1,37 @@
 import pyperclip
+import qbittorrent
+from os import environ
 from sys import argv
 
-separate, current_cb = ' ', ''
+separate, current_cb = ' ', ['']
+
+
+def add_torrents(links_list):
+    print('\n\n', 'sending to torrent box')
+    qb = qbittorrent.Client('http://192.168.1.144:8080/')
+    qb.login(environ['QBIT_NAME'], environ['QBIT_PW'])
+    [qb.download_from_link(ct) for ct in links_list]
+
 
 if '-n' in argv[1:]:
     separate = '\n\n'
+    print("new line")
 
-pyperclip.copy(current_cb)
+if '-c' in argv[1:]:
+    separate = ', '
+    print("comma separated")
+
+pyperclip.copy(current_cb[0])
 
 try:
     while True:
-        if current_cb != pyperclip.paste():
+        if current_cb[-1] != pyperclip.paste():
             print(pyperclip.paste())
-            current_cb += pyperclip.paste() + separate
-            pyperclip.copy(current_cb)
+            current_cb.append(pyperclip.paste())
 
 except KeyboardInterrupt:
-    print('\n\n', current_cb)
-    pyperclip.copy(current_cb)
+    if '-t' in argv[1:]:
+        add_torrents(current_cb)
+    print('\n\n', separate.join(current_cb))
+    pyperclip.copy(separate.join(current_cb))
 
