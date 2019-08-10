323b26c4b91a4dfb140a1e6cc28bc7671ad6216a
ssk8 <stinkyskate@pm.me>
2019-08-10 16:12:34 -0500

    continous update of list, end by cntr-c

--- clipper.py
+++ clipper.py
@@ -1,25 +1,21 @@
 import pyperclip
 from sys import argv
 
-cli_args = argv[1:]
+separate, current_cb = ' ', ''
 
-links = ['start']
-pyperclip.copy('start')
+if '-n' in argv[1:]:
+    separate = '\n\n'
 
-print('copy ---> end <--- to quit')
+pyperclip.copy(current_cb)
 
+try:
+    while True:
+        if current_cb != pyperclip.paste():
+            print(pyperclip.paste())
+            current_cb += pyperclip.paste() + separate
+            pyperclip.copy(current_cb)
 
-while links[-1] != 'end':
-    if links[-1] != pyperclip.paste():
-        links.append(pyperclip.paste())
-        print(links[-1])
-
-links = [link.strip() for link in links[1:-1]]
-
-if '-q' in cli_args:
-    links = [f'"{lnk}"' for lnk in links]
-
-pyperclip.copy(' '.join(links))
-
-print(' '.join(links))
+except KeyboardInterrupt:
+    print('\n\n', current_cb)
+    pyperclip.copy(current_cb)
 
