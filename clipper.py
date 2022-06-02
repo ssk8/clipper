6082b0fe9dcfabb1de2ec0eebdabaa61662cfffb
ssk8 <stinkyskate@pm.me>
2022-06-01 19:14:19 -0500

    added check current

--- clipper.py
+++ clipper.py
@@ -12,8 +12,23 @@ def input_thread(stop_list):
     stop_list.append(True)
 
 
+def check_clipboard():
+    if new_clip:=pyperclip.paste():
+        pyperclip.copy('')
+        return new_clip
+
+
+def start_up():
+    print("System clipboard to tbox. Press enter when done")
+    if (new_clip:=check_clipboard()) and len(new_clip)>10:
+        resp = input(f"But first, add current clipboard ({new_clip[:10]}...)? (default yes)")
+        if not resp.lower().startswith('n'):
+            pyperclip.copy(new_clip)
+
+
 def add_torrents(links_list):
-    print('attempting to send')
+    print('attempting to send:\n')
+    for link in links_list: print(link+'\n')
     qb = qbittorrentapi.Client(host=tbox_address, port=8080, username=environ['QBIT_NAME'], password=environ['QBIT_PW'])
     qb.auth_log_in() 
     qb.torrents_add(urls=links_list)
@@ -21,17 +36,16 @@ def add_torrents(links_list):
 
 
 def main():
-    print("press enter to send new clipboard coppies to tbox")
-    current_cb = ['']
-    pyperclip.copy(current_cb[0])
+    start_up()
+    mag_list = []
     stop_list = []
     _thread.start_new_thread(input_thread, (stop_list,))
     while not stop_list:
-        if current_cb[-1] != pyperclip.paste():
-            print(pyperclip.paste())
-            current_cb.append(pyperclip.paste())
+        if new_clip:=check_clipboard():
+            mag_list.append(new_clip)
+            print(f'added: {new_clip}')
 
-    add_torrents(current_cb[1:])
+    add_torrents(mag_list)
 
 if __name__ == "__main__":
     main()
